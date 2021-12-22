903c65e1df42282c65c0218926a729a894751b89
Carlos Rodriguez <crodveg@gmail.com>
2021-12-22 13:36:04 +0100

    trying to add test for submit tx

--- x/inter-tx/keeper/msg_server_test.go
+++ x/inter-tx/keeper/msg_server_test.go
@@ -2,6 +2,7 @@ package keeper_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
 	ibctesting "github.com/cosmos/ibc-go/v3/testing"
 
@@ -77,4 +78,101 @@ func (suite *KeeperTestSuite) TestRegisterInterchainAccount() {
 
 		})
 	}
+}
+
+func (suite *KeeperTestSuite) TestSubmitTx() {
+	var (
+		path *ibctesting.Path
+	)
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success", func() {
+			}, true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			icaAppA := suite.GetICAApp(suite.chainA)
+			icaAppB := suite.GetICAApp(suite.chainB)
+
+			owner := TestOwnerAddress // must be explicitly changed
+			addr, err := sdk.AccAddressFromBech32(owner)
+			suite.Require().NoError(err)
+
+			path = NewICAPath(suite.chainA, suite.chainB)
+			suite.coordinator.SetupConnections(path)
+
+			err = SetupICAPath(path, icaAppA, owner)
+			suite.Require().NoError(err)
+
+			portID, err := icatypes.GeneratePortID(TestOwnerAddress, ibctesting.FirstConnectionID, ibctesting.FirstConnectionID)
+			suite.Require().NoError(err)
+
+			// Get the address of the interchain account stored in state during handshake step
+			interchainAccountAddr, found := suite.GetICAApp(suite.chainA).ICAControllerKeeper.GetInterchainAccountAddress(suite.chainA.GetContext(), portID)
+			suite.Require().True(found)
+
+			icaAddr, err := sdk.AccAddressFromBech32(interchainAccountAddr)
+			suite.Require().NoError(err)
+
+			// Check if account is created
+			interchainAccount := icaAppB.AccountKeeper.GetAccount(suite.chainB.GetContext(), icaAddr)
+			suite.Require().Equal(interchainAccount.GetAddress().String(), interchainAccountAddr)
+
+			// Fund interchain account
+			suite.fundICAWallet(interchainAccountAddr, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10000))))
+
+			// Create bank transfer message to execute on the host
+			icaMsg := &banktypes.MsgSend{
+				FromAddress: interchainAccountAddr,
+				ToAddress:   suite.chainB.SenderAccount.GetAddress().String(),
+				Amount:      sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100))),
+			}
+
+			// Get initial balance of recipient account on host chain
+			initialBalance := icaAppB.BankKeeper.GetBalance(suite.chainB.GetContext(), suite.chainB.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
+
+			msgSrv := keeper.NewMsgServerImpl(icaAppA.InterTxKeeper)
+			msg, err := types.NewMsgSubmitTx(addr, icaMsg, path.EndpointA.ConnectionID, path.EndpointB.ConnectionID)
+			suite.Require().NoError(err)
+
+			res, err := msgSrv.SubmitTx(sdk.WrapSDKContext(suite.chainA.GetContext()), msg)
+			suite.Require().NoError(err)
+
+			// TODO
+
+			balance := icaAppB.BankKeeper.GetBalance(suite.chainB.GetContext(), suite.chainB.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
+			suite.Require().Equal(sdk.NewInt(100).Int64(), balance.Amount.Sub(initialBalance.Amount).Int64())
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Nil(res)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) fundICAWallet(interchainAccountAddr string, amount sdk.Coins) {
+	msgBankSend := &banktypes.MsgSend{
+		FromAddress: suite.chainB.SenderAccount.GetAddress().String(),
+		ToAddress:   interchainAccountAddr,
+		Amount:      amount,
+	}
+
+	res, err := suite.chainB.SendMsgs(msgBankSend)
+	suite.Require().NotEmpty(res)
+	suite.Require().NoError(err)
 }
